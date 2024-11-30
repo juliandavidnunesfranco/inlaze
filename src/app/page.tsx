0dc8eef630eac485912cb28be2d73d0b303586cb
juliandev <juliandavidnunesfranco@hotmail.es>
2024-11-30 01:02:51 -0500

    chore: (Configuracion para usar componentes con shadcn, next.js v15, typescript, tailwind)

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from '@/components/ModeToggle';
+
+export default function Home() {
+    return (
+        <div>
+            <ModeToggle />
+            <h2>Hola Mundo!</h2>
+        </div>
+    );
+}
