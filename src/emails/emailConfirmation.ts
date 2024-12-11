f5d93bf469bd9e87d021a3f18b2f3dca505022e9
juliandev <juliandavidnunesfranco@hotmail.es>
2024-12-11 03:58:37 -0500

    Cambios en url correo de confirmacion

--- src/emails/emailConfirmation.ts
+++ src/emails/emailConfirmation.ts
@@ -18,7 +18,7 @@ export const emailConfirmation = ({ username, email, token }: Props) => {
           <p>Le escribimos para confirmar que su cuenta ha sido creada con éxito. Ahora puede acceder a nuestro sistema.</p>
           <p>A continuación se muestra un enlace para confirmar su correo electrónico y poder acceder.</p>
           <ul>
-            <li><strong>Enlace:</strong><a href="http://localhost:3000/confirmation?email=${email}&token=${token}" target="_blank">Confirmar correo electrónico</a></li>
+            <li><strong>Enlace:</strong><a href="https://inlaze-pearl.vercel.app/confirmation?email=${email}&token=${token}" target="_blank">Confirmar correo electrónico</a></li>
           </ul>
           <p>Por favor, mantenga esta información en un lugar seguro y no la comparta con nadie.</p>
           <p>Si tiene alguna pregunta o necesita ayuda para comenzar, no dude en ponerse en contacto con nuestro equipo de soporte </p>
